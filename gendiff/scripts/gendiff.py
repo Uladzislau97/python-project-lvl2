d832fb2798837dd0307ec165380b96750d7496d7
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-01-04 00:15:39 +0300

    Update format argument description

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,7 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', dest='format', help='output format')
+    parser.add_argument('-f', '--format', dest='format', help='set format of output')
     parser.parse_args()
 
 
