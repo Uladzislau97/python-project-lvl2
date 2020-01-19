713379a182be0bd933919932a07493943b65b787
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-01-19 15:32:48 +0300

    Render diff in nested format

--- gendiff/formatters/renderer.py
+++ gendiff/formatters/renderer.py
@@ -1,2 +1,29 @@
+INDENT_LENGTH = 4
+
+
 def render_diff(diff_data, format):
-    return diff_data
+    indentation = ' ' * INDENT_LENGTH
+    nodes_representation = []
+    for node in diff_data:
+        if node['type'] == 'removed':
+            nodes_representation.append(
+                f"{indentation}  - {node['key']}: ${node['value']}"
+            )
+        elif node['type'] == 'added':
+            nodes_representation.append(
+                f"{indentation}  + {node['key']}: ${node['value']}"
+            )
+        elif node['type'] == 'unchanged':
+            nodes_representation.append(
+                f"{indentation}    {node['key']}: ${node['value']}"
+            )
+        elif node['type'] == 'updated':
+            nodes_representation.append(
+                f"{indentation}  - {node['key']}: ${node['old_value']}"
+            )
+            nodes_representation.append(
+                f"{indentation}  + {node['key']}: ${node['new_value']}"
+            )
+    content = '\n'.join(nodes_representation)
+    result = f"{'{'}\n{content}\n{'}'}"
+    return result
