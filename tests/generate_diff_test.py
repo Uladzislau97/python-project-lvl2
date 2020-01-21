19abcf7677a003f187812f9e0982d2791d6ae5bf
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-01-21 19:08:24 +0300

    Add tests for comparing of yaml files

--- tests/generate_diff_test.py
+++ tests/generate_diff_test.py
@@ -3,11 +3,20 @@ from gendiff import generate_diff
 
 
 def test_json_diff():
-    print(path.abspath('.'))
     with open(path.abspath('tests/fixtures/diff_1'), 'r') as f:
         expected_result = f.read()
         actual_result = generate_diff(
             path.abspath('tests/fixtures/json_1.json'),
             path.abspath('tests/fixtures/json_2.json')
+        )
+        assert actual_result == expected_result
+
+
+def test_yaml_diff():
+    with open(path.abspath('tests/fixtures/diff_1'), 'r') as f:
+        expected_result = f.read()
+        actual_result = generate_diff(
+            path.abspath('tests/fixtures/yaml_1.yml'),
+            path.abspath('tests/fixtures/yaml_2.yml')
         )
         assert actual_result == expected_result
