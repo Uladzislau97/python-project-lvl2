66f6413f3441c14ee0cede4d11046ebd595ae8b4
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-01-29 23:19:07 +0300

    Add tests for complex diff

--- tests/generate_diff_test.py
+++ tests/generate_diff_test.py
@@ -20,3 +20,23 @@ def test_yaml_diff():
             path.abspath('tests/fixtures/yaml_2.yml')
         )
         assert actual_result == expected_result
+
+
+def test_complex_json_diff():
+    with open(path.abspath('tests/fixtures/diff_2'), 'r') as f:
+        expected_result = f.read()
+        actual_result = generate_diff(
+            path.abspath('tests/fixtures/json_complex_1.json'),
+            path.abspath('tests/fixtures/json_complex_2.json')
+        )
+        assert actual_result == expected_result
+
+
+def test_complex_yaml_diff():
+    with open(path.abspath('tests/fixtures/diff_2'), 'r') as f:
+        expected_result = f.read()
+        actual_result = generate_diff(
+            path.abspath('tests/fixtures/yaml_complex_1.json'),
+            path.abspath('tests/fixtures/yaml_complex_2.json')
+        )
+        assert actual_result == expected_result
