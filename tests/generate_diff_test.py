9d7ffc50bd2f710db53dee795d15305fbf70a014
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-02-02 11:44:35 +0300

    Add test for plain format

--- tests/generate_diff_test.py
+++ tests/generate_diff_test.py
@@ -40,3 +40,14 @@ def test_complex_yaml_diff():
             path.abspath('tests/fixtures/yaml_complex_2.yml')
         )
         assert actual_result == expected_result
+
+
+def test_plain_diff():
+    with open(path.abspath('tests/fixtures/diff_3'), 'r') as f:
+        expected_result = f.read()
+        actual_result = generate_diff(
+            path.abspath('tests/fixtures/yaml_complex_1.yml'),
+            path.abspath('tests/fixtures/yaml_complex_2.yml'),
+            'plain'
+        )
+        assert actual_result == expected_result
