07e6ff9f191caef9a86f8f6a1900292d8e5bd540
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-02-08 23:34:42 +0300

    Refactor tests

--- tests/generate_diff_test.py
+++ tests/generate_diff_test.py
@@ -2,63 +2,62 @@ from os import path
 from gendiff import generate_diff
 
 
-def test_json_diff():
-    with open(path.abspath('tests/fixtures/diff_1'), 'r') as f:
+def compare(filepath1, filepath2, expected_result_filepath, format=None):
+    with open(path.abspath(expected_result_filepath), 'r') as f:
         expected_result = f.read()
         actual_result = generate_diff(
-            path.abspath('tests/fixtures/json_1.json'),
-            path.abspath('tests/fixtures/json_2.json')
+            path.abspath(filepath1),
+            path.abspath(filepath2),
+            format
         )
         assert actual_result == expected_result
+
+
+def test_json_diff():
+    compare(
+        'tests/fixtures/json_1.json',
+        'tests/fixtures/json_2.json',
+        'tests/fixtures/diff_1'
+    )
 
 
 def test_yaml_diff():
-    with open(path.abspath('tests/fixtures/diff_1'), 'r') as f:
-        expected_result = f.read()
-        actual_result = generate_diff(
-            path.abspath('tests/fixtures/yaml_1.yml'),
-            path.abspath('tests/fixtures/yaml_2.yml')
-        )
-        assert actual_result == expected_result
+    compare(
+        'tests/fixtures/yaml_1.yml',
+        'tests/fixtures/yaml_2.yml',
+        'tests/fixtures/diff_1'
+    )
 
 
 def test_complex_json_diff():
-    with open(path.abspath('tests/fixtures/diff_2'), 'r') as f:
-        expected_result = f.read()
-        actual_result = generate_diff(
-            path.abspath('tests/fixtures/json_complex_1.json'),
-            path.abspath('tests/fixtures/json_complex_2.json')
-        )
-        assert actual_result == expected_result
+    compare(
+        'tests/fixtures/json_complex_1.json',
+        'tests/fixtures/json_complex_2.json',
+        'tests/fixtures/diff_2'
+    )
 
 
 def test_complex_yaml_diff():
-    with open(path.abspath('tests/fixtures/diff_2'), 'r') as f:
-        expected_result = f.read()
-        actual_result = generate_diff(
-            path.abspath('tests/fixtures/yaml_complex_1.yml'),
-            path.abspath('tests/fixtures/yaml_complex_2.yml')
-        )
-        assert actual_result == expected_result
+    compare(
+        'tests/fixtures/yaml_complex_1.yml',
+        'tests/fixtures/yaml_complex_2.yml',
+        'tests/fixtures/diff_2'
+    )
 
 
 def test_plain_formated_diff():
-    with open(path.abspath('tests/fixtures/diff_3'), 'r') as f:
-        expected_result = f.read()
-        actual_result = generate_diff(
-            path.abspath('tests/fixtures/yaml_complex_1.yml'),
-            path.abspath('tests/fixtures/yaml_complex_2.yml'),
-            'plain'
-        )
-        assert actual_result == expected_result
+    compare(
+        'tests/fixtures/yaml_complex_1.yml',
+        'tests/fixtures/yaml_complex_2.yml',
+        'tests/fixtures/diff_3',
+        'plain'
+    )
 
 
 def test_json_formated_diff():
-    with open(path.abspath('tests/fixtures/diff_4'), 'r') as f:
-        expected_result = f.read()
-        actual_result = generate_diff(
-            path.abspath('tests/fixtures/yaml_complex_1.yml'),
-            path.abspath('tests/fixtures/yaml_complex_2.yml'),
-            'json'
-        )
-        assert actual_result == expected_result
+    compare(
+        'tests/fixtures/yaml_complex_1.yml',
+        'tests/fixtures/yaml_complex_2.yml',
+        'tests/fixtures/diff_4',
+        'json'
+    )
