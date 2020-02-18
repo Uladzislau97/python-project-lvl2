3bba2fb52271e2a128280bcffa3f93f1c522fa2b
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-02-18 19:17:12 +0300

    Refactor plain formatter

--- gendiff/format/plain.py
+++ gendiff/format/plain.py
@@ -2,25 +2,25 @@ from gendiff import node_types
 
 
 def stringify(data):
-    if type(data) is dict:
+    if isinstance(data, dict):
         return '[complex value]'
-    if type(data) is str:
+    if isinstance(data, str):
         return f"'{data}'"
-    if type(data) is bool:
+    if isinstance(data, bool):
         return str(data).lower()
     return str(data)
 
 
-def render_iter(data, parents=[]):
+def format(data, parents=()):
     nodes_representation = []
     for node in data:
         node_type = node['type']
         node_key = node['key']
-        name = '.'.join(parents + [node_key])
+        name = '.'.join(parents + (node_key,))
 
         if node_type == node_types.COMPLEX:
-            new_parents = parents + [node_key]
-            content = render_iter(node['children'], new_parents)
+            new_parents = parents + (node_key,)
+            content = format(node['children'], new_parents)
         elif node_type == node_types.ADDED:
             value = stringify(node['value'])
             content = f"Property '{name}' was added with value: {value}"
@@ -29,15 +29,13 @@ def render_iter(data, parents=[]):
         elif node_type == node_types.UPDATED:
             old_value = stringify(node['old_value'])
             new_value = stringify(node['new_value'])
-            content = f"Property '{name}' was updated. " + \
+            content = (
+                f"Property '{name}' was updated. "
                 f"From {old_value} to {new_value}"
+            )
         else:
             continue
 
         nodes_representation.append(content)
 
     return '\n'.join(nodes_representation)
-
-
-def format(diff_data):
-    return render_iter(diff_data)
